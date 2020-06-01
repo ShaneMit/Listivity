8af5f597de42a1b34218932531ad890cb8fd4b22
Erika <erika.paige.handley@gmail.com>
2020-06-01 11:33:39 -0700

    server file created

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const { join } = require('path')
+
+const app = express()
+
+app.use(express.static(join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+// app.use(require('./routes'))
+
+require('./connection')
+  // .sync({ force: true })
+  .sync()
+  .then(() => app.listen(3000, () => console.log('http://localhost:3000')))
+  .catch(err => console.error(err))
