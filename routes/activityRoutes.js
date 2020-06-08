fb40f5295e80871adfb4286b4c68c41f5154cdb6
Erika <erika.paige.handley@gmail.com>
2020-06-08 13:00:15 -0700

    commented out Routes & added ; to end of lines

--- routes/activityRoutes.js
+++ routes/activityRoutes.js
@@ -1,5 +1,5 @@
-const router = require('express').Router()
-const { User, Activity, Eat, Entertain } = require('../models')
+const router = require('express').Router();
+const { User, Activity, Eat, Entertain } = require('../models');
 
 //CRUD APPLICATION ROUTES BELOW
 // GET all activities
@@ -8,32 +8,35 @@ router.get('/activities', (req, res) => {
   Activity.findAll()
     .then(activities => res.json(activities))
     .catch(err => console.error(err))
-})
+});
 
+// GET activities based on user's uniqueId 
 router.get('/activities/:id', (req, res) => {
   Activity.findOne({ id: req.params.id })
     .then(activity => res.json(activity))
     .catch(err => console.error(err))
-})
+});
 
+// ADD an activity 
 router.post('/activities', (req, res) => {
   Activity.create(req.body)
     .then((activity) => res.json(activity))
     .catch(err => console.error(err))
-})
+});
 
+// EDIT an activity in a user's database
 router.put('/activities/:id', (req, res) => {
   Activity.update(req.body, { where: { id: req.params.id } })
     .then(() => res.sendStatus(200))
     .catch(err => console.error(err))
-})
+});
 
-// DELETE one user
+// DELETE one activity
 router.delete('/activities/:id', (req, res) => {
   Activity.destroy({ where: { id: req.params.id } })
     .then(() => res.sendStatus(200))
     .catch(err => console.error(err))
-})
+});
 
 //export router out
 module.exports = router
