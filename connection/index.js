0f613c5bc9189a2e92882a64b405f5d5635f1adf
ShaneMit <shanemit@cox.net>
2020-06-08 15:27:20 -0700

    Edited sequelize

--- connection/index.js
+++ connection/index.js
@@ -1,5 +1,7 @@
+const mysql = require('mysql2')
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize(mysql.createConnection(process.env.JAWSDB_URL || process.env.LOCAL_URL))
+const sequelize = new Sequelize(process.env.JAWSDB_URL || process.env.LOCAL_URL)
+
 
 module.exports = sequelize
